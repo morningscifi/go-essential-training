404a3133bdaf1bb4eed2559ec48f4876842124ee
morningscifi <morningscifi@users.noreply.github.com>
2023-02-07 20:34:18 -0500

    max function

--- Ch02/02_10/max.go
+++ Ch02/02_10/max.go
@@ -8,4 +8,16 @@ import (
 func main() {
 	nums := []int{16, 8, 42, 4, 23, 15}
 	fmt.Println(nums)
+	max_index := find_largest(nums)
+	fmt.Printf("Nums[%d]= %d", max_index, nums[max_index])
+}
+
+func find_largest(nums []int) int {
+	idx := 0
+	for i, val := range nums {
+		if val >= nums[idx] {
+			idx = i
+		}
+	}
+	return idx
 }

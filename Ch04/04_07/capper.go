01e7666befde61356fda30209cd821dba3e597b7
morningscifi <morningscifi@users.noreply.github.com>
2023-02-08 22:04:10 -0500

    Finish Capper.go

--- Ch04/04_07/capper.go
+++ Ch04/04_07/capper.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 // Capper implements io.Writer and turns everything to uppercase
 type Capper struct {
-	// TODO
+	wtr io.Writer
+}
+
+func (c *Capper) Write(p []byte) (n int, err error) {
+	buffer := make([]byte, len(p))
+	for k, v := range p {
+		if v >= 'a' && v <= 'z' {
+			v -= 32
+		}
+		buffer[k] = v
+	}
+	return c.wtr.Write(buffer)
 }
 
 func main() {

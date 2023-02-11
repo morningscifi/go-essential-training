be293969da62945852063a2feced70622db24184
morningscifi <morningscifi@users.noreply.github.com>
2023-02-11 14:27:09 -0500

    Learn how channels work
    And how to read from a file

--- Ch05/05_03/kill.go
+++ Ch05/05_03/kill.go
@@ -3,18 +3,40 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+
+	"github.com/pkg/errors"
 )
 
 func killServer(pidFile string) error {
-	// TODO
+	// Open the file which contains the string of a PID
+	file, err := os.Open(pidFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-	// Simulate kill
+	var pid int
+	// Pretend to read the pid and convert it to an int
+	// Value is thrown away, since there's no real process
+	// to kill
+	if _, err := fmt.Fscanf(file, "%d", &pid); err != nil {
+		return errors.Wrap(err, "bad process ID")
+	}
+
+	// Simulate killing the process
 	fmt.Printf("killing server with pid=%d\n", pid)
+
+	if err := os.Remove(pidFile); err != nil {
+		log.Printf("warning: can't remove pid file - %s", err)
+	}
+
 	return nil
 }
 
 func main() {
 	if err := killServer("server.pid"); err != nil {
-		log.Fatalf("error: %s", err)
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		os.Exit(1)
 	}
 }
--- Ch06/06_03/sites.go
+++ Ch06/06_03/sites.go
@@ -6,16 +6,18 @@ import (
 	"net/http"
 )
 
-func returnType(url string) {
+func returnType(url string, ch chan<- string) {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Printf("%s -> error: %s\n", url, err)
+		ch <- fmt.Sprintf("%s -> error: %s\n", url, err)
 		return
 	}
-
+	// Should quiz myself on the difference between:
+	// Sprintf, Printf, Fscanf, etc.
 	defer resp.Body.Close()
 	ctype := resp.Header.Get("content-type")
 	fmt.Printf("%s -> %s", url, ctype)
+	ch <- ctype
 }
 
 func main() {
@@ -28,8 +30,12 @@ func main() {
 	// Create response channel
 	ch := make(chan string)
 	for _, url := range urls {
-		go returnType(url)
+		go returnType(url, ch)
 	}
 
 	// TODO: Wait using channel
+	for i := range urls {
+		output := <-ch
+		fmt.Printf("\n%d Output: %s\n", i, output)
+	}
 }

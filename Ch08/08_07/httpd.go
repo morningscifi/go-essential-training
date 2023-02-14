79d28c6e60f3c1f6ff941d45f90903348b7d1755
morningscifi <morningscifi@users.noreply.github.com>
2023-02-14 16:04:43 -0500

    unfinished httpd

--- Ch08/08_07/httpd.go
+++ Ch08/08_07/httpd.go
@@ -1,4 +1,5 @@
-/* Key/Value database
+/*
+	Key/Value database
 
 $ curl -d'hello' http://localhost:8080/k1
 $ curl http://localhost:8080/k1
@@ -11,6 +12,9 @@ Limit value size to 1k
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -18,12 +22,52 @@ type Server struct {
 	db DB
 }
 
+type dbResponse struct {
+	Error string `json:"error"`
+	Data  []byte `json:"data"`
+}
+
+type dbRequest struct {
+	Key string `json:"key"`
+}
+
 // POST /key Store request body as value
 // GET /<key> Send back value, or 404 if key not found
-func (s *Server) Handler(w http.ResponseWriter, r *http.Request) {
-	// TODO
+func (s *Server) getHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	var dbr dbRequest
+	dec := json.NewDecoder(r.Body)
+
+	fmt.Printf("Received:\n%v#+", r.Body)
+
+	if err := dec.Decode(&dbr); err != nil {
+		http.Error(w, "decode failed", http.StatusBadRequest)
+		log.Printf("error: %s", err)
+		return
+	}
+
+	resp := dbResponse{Error: "", Data: s.db.Get(dbr.Key)}
+	// Encode result
+	w.Header().Set("Content-Type", "application/json")
+	if resp.Error != "" {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	enc := json.NewEncoder(w)
+	if err := enc.Encode(&resp); err != nil {
+		log.Printf("can't encode %v - %s", resp, err)
+	}
 }
 
 func main() {
 	// TODO: Routing & start server
+	var s Server
+	s.db.Set("yo", []byte("dog"))
+	http.HandleFunc("/get", s.getHandler)
+
+	addr := ":8080"
+	log.Printf("server ready on %s", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatal(err)
+	}
 }
